9f912e280f5bff42b2721e3036b6be1c7b95f4df
Charity Njoroge <charity.njoroge@student.moringaschool.com>
2024-05-23 16:06:18 +0300

    update

--- lib/sequences.py
+++ lib/sequences.py
@@ -11,11 +11,10 @@ def print_fibonacci(length):
         print("[0, 1]")
         return
     
-    fibonacci_sequence = [0,1] 
+    fibonacci_sequence = [0,1]
     while len(fibonacci_sequence) < length:
         next_number = fibonacci_sequence[-1] + fibonacci_sequence[-2]
         fibonacci_sequence.append(next_number)
-
-
     print(fibonacci_sequence)
 
+print_fibonacci(10)
